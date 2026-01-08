2afce1b6f0e7cd3a212d45b2a93b890ebafdf4b7
stevenMarmion <marmionsteven07@gmail.com>
2026-01-08 18:55:16 +0100

    fix(refactoring) : Move files in correct package

---
+++ src/main/java/com/esiea/monstre/poche/models/status/terrain/Asseche.java
@@ -0,0 +1,16 @@
+package com.esiea.monstre.poche.models.status.terrain;
+
+import com.esiea.monstre.poche.models.battle.logs.CombatLogger;
+import com.esiea.monstre.poche.models.core.Terrain;
+
+public class Asseche extends StatutTerrain {
+
+    public Asseche() {
+        this.labelStatut = "Asseche";
+    }
+
+    public void appliquerEffets(Terrain terrain) {
+        terrain.setStatutTerrain(new Asseche());
+        CombatLogger.log(terrain.getNomTerrain() + " est maintenant asséché.");
+    }
+}
